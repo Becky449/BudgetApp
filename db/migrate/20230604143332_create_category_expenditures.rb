62e3c032dbb60d239de14e4bfe8910369554d587
Becky449 <rabnab12@gmail.com>
2023-06-09 23:30:15 +0300

    Generate user model and did migrations

---
+++ db/migrate/20230604143332_create_category_expenditures.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateCategoryExpenditures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :category_expenditures do |t|
+      t.references :category, null: false, foreign_key: true
+      t.references :expenditure, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
